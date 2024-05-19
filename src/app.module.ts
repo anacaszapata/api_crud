43d10e29b131e0807f9b8e7367aee968a2393d71
anacaszapata <anacaszapata@gmail.com>
2024-05-18 20:03:10 -0500

    fix(add environment variables).env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { HeroesModule } from './heroes/heroes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HeroesController } from './heroes/heroes.controller';
 import { HeroesService } from './heroes/heroes.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'user_crud',
-      password: 'root',
-      database: 'db_crud',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
